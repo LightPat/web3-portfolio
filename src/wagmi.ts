8ca48cad5ef8fabd2c1ef52b15e9aba5545361e2
Patrick Seeman <patseem189@gmail.com>
2026-02-28 20:12:08 -0500

    use foundry and sepolia

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -6,17 +6,15 @@ import {
   optimism,
   polygon,
   sepolia,
+  foundry
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
   chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
+    sepolia,
+    foundry,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
   ],
   ssr: true,
