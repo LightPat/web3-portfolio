24eb3c79f2d82a957321f61e2fe8897ce1da21a6
Patrick Seeman <patseem189@gmail.com>
2026-02-28 22:46:39 -0500

    basic website setup

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -10,8 +10,8 @@ import {
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+  appName: 'Web3-Portfolio',
+  projectId: '448fcf93b2a9c65beeac097214c050d3',
   chains: [
     sepolia,
     foundry,
